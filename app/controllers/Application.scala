ab7602ced97589ee64e88522cdb013297629b73b
shun-fz <shun-fz@rontech.co.jp>
2014-10-01 08:37:05 +0000

    Add json function

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,21 +8,17 @@ import play.api.data.Forms._
 import play.api.libs.json.Json
 import play.api.libs.functional.syntax._
 
-case class ReceiveWord (
-  wordData: String
-)
-
 object Application extends Controller {
-
-  implicit val jsonReads = Json.format[ReceiveWord]
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
   def receive = Action { implicit request =>
-    val data = request.body.asFormUrlEncoded
-    val sendMsg = """{ status: """ + data.get + """ } """
+    val data = request.body.asFormUrlEncoded.get
+    val ms1:List[String] = data.get("text1").get.toList
+    val ms2:List[String] = data.get("text2").get.toList
+    val sendMsg = "{" + ms2.head + ":"  + ms1.head + "}"
     val json = Json.toJson(sendMsg)
 
     Ok(json)
